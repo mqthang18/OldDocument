3b1a028751a3894e6b71ac84d7a8b86cfa302cb6
mqthang_18sh <maiquocthangt@gmail.com>
2021-12-12 23:15:08 +0700

    Update menubar

--- source/views/blocks/MenuBar.js
+++ source/views/blocks/MenuBar.js
@@ -2,13 +2,13 @@ const MenuBar = `
     <div class="col-sm-3 sidenav">
         <h4>Ôn thi học kì</h4>
         <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="?topic=CHSH">Chuyển hóa sinh học</a></li>
-        <li><a href="?topic=CNLM">Công nghệ lên men</a></li>
-        <li><a href="?topic=CNTP">Công nghệ thực phẩm</a></li>
-        <li><a href="?topic=DTVSV">Di truyển vi sinh vật</a></li>
-        <li><a href="?topic=VSTS">Vi sinh thủy sản</a></li>
-        <li><a href="?topic=VSVTY">Vi sinh vật thú y</a></li>
-        <li><a href="?topic=VSVYH">Vi sinh vật y học</a></li>
+        <li class="active"><a href="index.html?topic=CHSH">Chuyển hóa sinh học</a></li>
+        <li><a href="index.html?topic=CNLM">Công nghệ lên men</a></li>
+        <li><a href="index.html?topic=CNTP">Công nghệ thực phẩm</a></li>
+        <li><a href="index.html?topic=DTVSV">Di truyển vi sinh vật</a></li>
+        <li><a href="index.html?topic=VSTS">Vi sinh thủy sản</a></li>
+        <li><a href="index.html?topic=VSVTY">Vi sinh vật thú y</a></li>
+        <li><a href="index.html?topic=VSVYH">Vi sinh vật y học</a></li>
         </ul><br>
         <!-- <div class="input-group">
         <input type="text" class="form-control" placeholder="Search Blog..">
