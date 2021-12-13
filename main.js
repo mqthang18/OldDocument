86058a4a664178fddba6e8049c53c73666f9e12e
mqthang_18sh <maiquocthangt@gmail.com>
2021-12-13 09:52:31 +0700

    Update

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ var menubarBlock = new Vue({
     el: '#menubar',
     template: menubarT
 })
+
+
 try {MenuBarF(topic);} 
 catch (error) {
     window.location.href=url;
@@ -24,10 +26,20 @@ var PostBlock = new Vue({
     data: {
         title:titleT,
         listBlogs: dataT
+    },
+    methods: {
+        scrollToBottom(e,id) {
+            console.log(id)
+            e.preventDefault();
+            var element = document.getElementById(id);
+
+            // var top = element.offsetTop;
+            var y = element.offsetTop; 
+            // console.log(y)
+            // window.scrollTo(0, top);
+            // element.scrollIntoView({behavior: "smooth", block: "end"});
+            window.scrollTo({top: y, behavior: 'smooth'});
+        }
     }
 })
 
-// var app = new Vue({
-//     el: '#app',
-//     template: data
-// })
--- source/views/blocks/Post.js
+++ source/views/blocks/Post.js
@@ -2,10 +2,10 @@ const Post = `
     <div class="post">
         <h2>{{ title }}</h2>
         <ul style="list-style-type:none; background: #e1e1ff; padding: 5px">
-            <li v-for="abc in listBlogs"><a v-bind:href="'#'+abc.number">{{ abc.question }} - Giảng viên: {{ abc.author }} </a></li>
+            <li v-for="abc in listBlogs"><a v-on:click="scrollToBottom($event,'block'+abc.number)" v-bind:href="'#'+abc.number">{{ abc.question }} - Giảng viên: {{ abc.author }} </a></li>
         </ul>
-        <div v-for="abc in listBlogs">
-            <h4  v-bind:id="abc.number" >{{ abc.question }}</h4>
+        <div v-for="abc in listBlogs" v-bind:id="'block'+abc.number">
+            <h4 v-bind:id="abc.number" >{{ abc.question }}</h4>
             <span class="label label-danger">Công nghệ sinh học</span> <span class="label label-primary">{{ abc.subject }}</span>
             <p style="text-align:right;">Câu hỏi của giảng viên: {{ abc.author }}</p>
             <p style="text-align:justify; overflow: auto; background-color: #e7e6e6; padding: 5px;" v-html="abc.answer"></p>
